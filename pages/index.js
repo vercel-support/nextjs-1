432dc701a51128808ad2d4f5dfa07c7f2479d9a2
samsisle <samko9522@gmail.com>
2021-03-18 15:57:36 -0700

    Force commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>BasePath Image Bug</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
